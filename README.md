b49779aa5ad2b519608559323f394bcdf391b261
Bidur Adhikari <badhikari@godaddy.com>
2018-04-12 13:49:05 -0700

    Implemented 2 counters and added new todo

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # redux_counter
 - Counter using redux
 - To do
-    - Have 2 counters.
+    - Have 2 counters.(done)
+    - Have incrementBy TextBox which determines how much to increment.
--- src/index.js
+++ src/index.js
@@ -4,17 +4,26 @@ import { createStore } from 'redux'
 import Counter from './components/Counter'
 import counter from './reducers'
 
-const store = createStore(counter)
+const store1 = createStore(counter)
+const store2 = createStore(counter)
 const rootEl = document.getElementById('root')
 
 const render = () => ReactDOM.render(
+  <div>
   <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-  />,
+    value={store1.getState()}
+    onIncrement={() => store1.dispatch({ type: 'INCREMENT' })}
+    onDecrement={() => store1.dispatch({ type: 'DECREMENT' })}
+  />
+   <Counter
+    value={store2.getState()}
+    onIncrement={() => store2.dispatch({ type: 'INCREMENT' })}
+    onDecrement={() => store2.dispatch({ type: 'DECREMENT' })}
+  />
+  </div>,
   rootEl
 )
 
 render()
-store.subscribe(render)
+store1.subscribe(render)
+store2.subscribe(render)
